d275a1b1755b7e02535774bd9d94e1abeebd5d95
Xavier Juvigny <juvigny@onera.fr>
2018-02-15 15:40:45 +0100

    Simplification interface pour liste d'initialisation

--- solutions/2_POO/cloud_of_points_v2/test_cloud_of_points.cpp
+++ solutions/2_POO/cloud_of_points_v2/test_cloud_of_points.cpp
@@ -3,8 +3,8 @@ using namespace Geometry;
 
 int main()
 {
-    CloudOfPoints cld{ Point{1.,0.,0.}, Point{0.,1.,0.}};
-    CloudOfPoints cld2{ Point{0.,0.,1.}, Point{1.,0.,1.}};
+    CloudOfPoints cld( { {1.,0.,0.}, {0.,1.,0.} } );
+    CloudOfPoints cld2( { {0.,0.,1.}, {1.,0.,1.} } );
     CloudOfPoints cld3 = cld + cld2;
     std::cout << cld << "\n" << std::string(cld) << std::endl;
     std::cout << std::string(cld3) << std::endl;
